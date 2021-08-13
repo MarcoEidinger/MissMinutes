6c16578041a8da0777293768f9567aaae8c60ef3
Marco Eidinger <marco.eidinger@sap.com>
2021-08-13 08:44:48 -0700

    Miss Minute easer egg (Youtube Video)
    when clicking on Miss Minute image

--- MissMinutes/Views/ContentView.swift
+++ MissMinutes/Views/ContentView.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TimezonesView()
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        HStack {
+            Image("MissMinutes").resizable().frame(width: 100, height: 100, alignment: .center).onTapGesture {
+                NSWorkspace.shared.open(URL(string: "https://www.youtube.com/watch?v=9ZcyoZlY0aU")!)
+            }
+            TimezonesView()
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
 
